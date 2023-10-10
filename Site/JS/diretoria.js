b8bf57913768fbbe7bf6c6bf8b61897395c09640
FellypeGC <webpulse479@gmail.com>
2023-10-10 18:13:29 -0300

    javascript adicionado novamente

--- Site/JS/diretoria.js
+++ Site/JS/diretoria.js
@@ -1,4 +1,8 @@
-let 
+let menuToggle = document.querySelector('.menuToggle');
+let header = document.querySelector('header');
+menuToggle.onclick = function() {
+    header.classList.toggle('active');
+}
 
 
 
